77e20b89f8606c98cd240c331e93bb67089a0aee
Anton Schwarz <anton@mendoza-schwarz.de>
2024-03-27 16:41:51 +0000

    Removed tags from tests
    got it up and running on different machine now

--- test/core/test_bill.py
+++ test/core/test_bill.py
@@ -6,7 +6,7 @@ from core.bill import Bill
 class TestBills:
     def test_failed_creation(self):
         with pytest.raises(TypeError) as e:
-            Bill()
+            Bill()  # type: ignore
 
     def test_creation(self):
         Bill("test", datetime.now())
